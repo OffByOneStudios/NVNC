4d89a776c5c3023a714676f35c5af9770781a7ee
T1T4N <T1T4N@localhost>
2012-08-08 03:59:36 +0200

    First CodePlex Mercurial commit ...

--- [CSharp]NVNC/Program.cs
+++ [CSharp]NVNC/Program.cs
@@ -9,7 +9,7 @@ namespace LVNC
     {
         static void Main(string[] args)
         {
-            VncServer s = new VncServer("T1T4N", 5900, "T!T@N - VNC");
+            VncServer s = new VncServer("T1T4N", 5900, "T!T@N-VNC");
             s.Start();
             Console.ReadLine();
         }
