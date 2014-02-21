c62eb067948b52228fb4f85d5425b0c6f40c58eb
T1T4N <T1T4N@localhost>
2014-02-21 15:45:26 +0100

    Update to version 1.5, including Zlib compression and Thread tests

---
+++ [VB.NET]ThreadTest/Module1.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Threading
+Imports NVNC
+Module Module1
+    Public Sub IniciarVNC()
+        Try
+            Dim myVnc As New NVNC.VncServer
+            myVnc.Name = "crosemffet"
+            myVnc.Password = "1234"
+            myVnc.Port = "5900"
+            myVnc.Start()
+
+        Catch ex As Exception
+            System.Diagnostics.Debug.WriteLine(ex.Message)
+            System.Threading.Thread.Sleep(1000)
+        End Try
+    End Sub
+    Sub Main()
+        Dim t2 As Thread
+        'port is closed, must open it
+        t2 = New Thread(AddressOf IniciarVNC)
+        t2.IsBackground = True
+        t2.Priority = ThreadPriority.Highest
+        t2.Start()
+        Console.ReadLine()
+    End Sub
+End Module
